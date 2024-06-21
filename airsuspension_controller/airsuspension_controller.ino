8f4d4d0aba1cb7ca7ed1e2cdc0c9c585fc783017
gopro2027 <gopro2027@gmail.com>
2024-06-21 10:07:48 -0400

    Set sda and scl pins for screen

--- airsuspension_controller/airsuspension_controller.ino
+++ airsuspension_controller/airsuspension_controller.ino
@@ -14,7 +14,7 @@
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
 #define TEST_MODE false
-#define SCREEN_MOODE false
+#define SCREEN_MOODE true
 
 // Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
 // The pins for I2C are defined by the Wire-library. 
@@ -142,6 +142,11 @@ bool getRaiseOnPressureSet() {
 }
 
 void setupSolenoidPins() {
+
+  #if SCREEN_MOODE == true
+    Wire.setPins(21,22); // sda, scl
+  #endif
+
   pinMode(solenoidFrontPassengerInPin, OUTPUT);
   pinMode(solenoidFrontPassengerOutPin, OUTPUT);
   pinMode(solenoidRearPassengerInPin, OUTPUT);
