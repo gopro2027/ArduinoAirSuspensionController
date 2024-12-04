ecf632c24405238dbe3bea10e53f532e5b31e058
gopro2027 <gopro2027@gmail.com>
2024-12-04 09:13:33 -0500

    Added ps3 library to platformio and renamed ps3 controller file added demo file

--- OASMan_ESP32/src/bluetooth/ps3.cpp
+++
@@ -1,5 +0,0 @@
-#include "ps3.h"
-
-#if ENABLE_PS3_CONTROLLER_SUPPORT
-
-#endif
--- OASMan_ESP32/src/bluetooth/ps3_controller.h
+++ OASMan_ESP32/src/bluetooth/ps3_controller.h
@@ -1,9 +1,11 @@
-#ifndef ps3_h
-#define ps3_h
+#ifndef ps3_controller_h
+#define ps3_controller_h
 #include "user_defines.h"
 #include "airSuspensionUtil.h"
 
 #if ENABLE_PS3_CONTROLLER_SUPPORT
+
+#include <Ps3Controller.h>
 
 #endif
 
