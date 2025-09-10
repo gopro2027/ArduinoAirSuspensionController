b69ceec7b5f78dad4ed1f523ff685d06bf32407e
gopro2027 <gopro2027@gmail.com>
2025-09-10 02:39:41 -0400

    converted it all back to older version of smartdisplay
    also fixed bug with the msgbox by increasing the memory size in lvgl

--- Wireless_Controller/include/lv_conf.h
+++ Wireless_Controller/include/lv_conf.h
@@ -45,9 +45,9 @@
 #define LV_USE_STDLIB_SPRINTF   LV_STDLIB_BUILTIN
 
 
-#if LV_USE_STDLIB_MALLOC == LV_STDLIB_BUILTI
+#if LV_USE_STDLIB_MALLOC == LV_STDLIB_BUILTIN
     /*Size of the memory available for `lv_malloc()` in bytes (>= 2kB)*/
-    #define LV_MEM_SIZE (64 * 1024U)          /*[bytes]*/ // this freaks tf out if i lower it. Random things stop working.
+    #define LV_MEM_SIZE (64 * 1024U * 2)          /*[bytes]*/ // this freaks tf out if i lower it below (64 * 1024U). Random things stop working. UPDATE: increased *2 to fix the issue with msgbox not working when we switched back to builtin. Using builtin stores the memory in a different area than CLIB
 
     /*Size of the memory expand for `lv_malloc()` in bytes*/
     #define LV_MEM_POOL_EXPAND_SIZE 0
